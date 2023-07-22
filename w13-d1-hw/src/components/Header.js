2af8dc30375e48c0f399ec0e4dd9947b00d22838
Je Yun <alwaysblue21@gmail.com>
2023-07-21 20:08:48 -0400

    header

--- w13-d1-hw/src/components/Header.js
+++ w13-d1-hw/src/components/Header.js
@@ -1,3 +1,15 @@
-export default function Header() {
-    return <h1>Header</h1>
+export default function Header(props) {
+    return (
+        <div className="Header">
+            <div className="MySelf">
+                <h2 className="MySelf-Name">Je Min Yun</h2>
+                <p className="MySelf-Position">Full-Stack Software Engineer</p>
+            </div>
+
+            <ul className="Nav-Bar">
+                <li><a href="https://github.com/alwaysblue21">GitHub Link</a></li>
+                <li><a href="https://www.linkedin.com/in/je-min-yun/">LinkedIn Link</a></li>
+            </ul>
+        </div>
+    )
 };
